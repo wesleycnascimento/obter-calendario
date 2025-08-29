9938eb86e4d57b85b134dd851695bad393b64873
WESLEY CARLOS NASCIMENTO <wesley.nascimento@bradesco.com.br>
2025-08-28 21:55:27 -0300

    Nova versão utilizando key vault.

--- src/main/java/com/example/obtercalendario/ObterCalendarioController.java
+++ src/main/java/com/example/obtercalendario/ObterCalendarioController.java
@@ -1,26 +1,44 @@
 package com.example.obtercalendario;
 
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.security.keyvault.secrets.SecretClient;
+import com.azure.security.keyvault.secrets.SecretClientBuilder;
+import com.azure.security.keyvault.secrets.models.KeyVaultSecret;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.Map;
+import java.time.format.DateTimeFormatter;
 
 @RestController
 public class ObterCalendarioController {
 
+    private final SecretClient secretClient;
+
+    @Value("${azure.keyvault.uri}")
+    private String keyVaultUri;
+
+    public ObterCalendarioController() {
+        this.secretClient = new SecretClientBuilder()
+                .vaultUrl(keyVaultUri)
+                .credential(new DefaultAzureCredentialBuilder().build())
+                .buildClient();
+    }
+
     @GetMapping("/obterCalendario")
-    public Map<String, Integer> obterCalendario() {
-        LocalDate today = LocalDate.now();
+    public String getCalendario() {
+        String name;
+        try {
+            KeyVaultSecret secret = secretClient.getSecret("PersonName");
 
-        Map<String, Integer> response = new HashMap<>();
+            name = secret.getValue();
+        } catch (Exception e) {
+            name = "Unknown user (secret not found)";
+        }
 
-        //Setando os valores de dia, mes e ano V1
-        response.put("dia", today.getDayOfMonth());
-        response.put("mes", today.getMonthValue());
-        response.put("ano", today.getYear());
+        String date = LocalDate.now().format(DateTimeFormatter.ofPattern("MM/dd/yyyy"));
 
-        return response;
+        return String.format("%s, today is %s", name, date);
     }
 }
